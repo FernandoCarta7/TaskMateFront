4dff81f3d2ff651ba0a6a81a8d3661cef049b096
Fernando Carta <cartafernando7@gmail.com>
2024-12-08 22:49:17 -0500

    CRUD Materias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,13 @@ import { InicioComponent } from './inicio/inicio.component';
 import { ListarActividadesComponent } from './ACTIVIDADES/listar-actividades/listar-actividades.component';
 import { EditarActividadesComponent } from './ACTIVIDADES/editar-actividades/editar-actividades.component';
 import { RegistrarActividadesComponent } from './ACTIVIDADES/registrar-actividades/registrar-actividades.component';
-import { EditarMateriasComponent } from './editar-materias/editar-materias.component';
-import { ListarMateriasComponent } from './listar-materias/listar-materias.component';
-import { RegistrarMateriasComponent } from './registrar-materias/registrar-materias.component';
-import { ListarPendientesComponent } from './listar-pendientes/listar-pendientes.component';
-import { EditarPendientesComponent } from './editar-pendientes/editar-pendientes.component';
-import { RegistrarPendientesComponent } from './registrar-pendientes/registrar-pendientes.component';
 import { ListarSemestresComponent } from './SEMESTRE/listar-semestres/listar-semestres.component';
 import { RegistrarSemestresComponent } from './SEMESTRE/registrar-semestres/registrar-semestres.component';
 import { EditarSemestresComponent } from './SEMESTRE/editar-semestres/editar-semestres.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { ListarMateriasComponent } from './MATERIAS/listar-materias/listar-materias.component';
+import { EditarMateriasComponent } from './MATERIAS/editar-materias/editar-materias.component';
+import { RegistrarMateriasComponent } from './MATERIAS/registrar-materias/registrar-materias.component';
 
 export const routes: Routes = [
     { path: 'registro-usuario',     component: RegistroUsuarioComponent,        'title' : 'Registro' },
@@ -31,9 +28,7 @@ export const routes: Routes = [
     { path:'registrar-materias',    component: RegistrarMateriasComponent,      'title':  'Registrar materias'},
     
     //RUTAS ASOCIADAS A PENDIENTES
-    { path:'listar-pendientes',     component: ListarPendientesComponent,       'title': 'Pendientes' },
-    { path:'editar-pendientes',     component: EditarPendientesComponent,       'title': 'Editar pendiente' },
-    { path:'registrar-pendientes',  component: RegistrarPendientesComponent,    'title':  'Registrar pendiente'},
+    
     
     //RUTAS ASOCIADAS A SEMESTRE
     { path:'listar-semestres',      component: ListarSemestresComponent,        'title': 'Semestre' },
@@ -42,5 +37,5 @@ export const routes: Routes = [
 
     { path:'inicio-sesion',         component: InicioSesionComponent,           'title':  'Iniciar sesión'},    
     { path: 'inicio',               component: InicioComponent,                 'title' : 'Inicio' },
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+    { path: '', redirectTo: 'inicio-sesion', pathMatch: 'full' }
 ];
--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../modelo/Usuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'inicio-sesion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
 export class InicioSesionComponent {
+  usuario: Usuario;
+  correo: string;
+  password: string;
+
+  constructor(private router : Router){}
+
+
+  login(){
+    this.iniciarSesion();
+  }
+ngOnInit(){
+  this.usuario = new Usuario("","","","","",null);
+}
+
+  iniciarSesion(){
+
+  }
+  irRegistroUsuario(){
+    this.router.navigate(['registro-usuario']);
+  }
 
 }
